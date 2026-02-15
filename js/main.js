8a24ce19b6e6ca192f659836f25f7aa0fbedf45e
Vishu Kamble <vishu.mb@gmail.com>
2026-02-15 09:17:03 -0600

    new website

--- js/main.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-    /* Github Activity Feed - https://github.com/vishukamble/github-activity */
-    GitHubActivity.feed({
-        username: "vishukamble",
-        selector: "#feed",
-        limit: 20 // optional
-    });
-});
